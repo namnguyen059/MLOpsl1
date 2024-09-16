6eb986894619ac0bacd63f15a1d7c7ea158dc578
Nam <namxyz012@gmail.com>
2024-09-16 11:36:37 +0700

    Add files via upload

--- dags/house_price_dag.py
+++ dags/house_price_dag.py
@@ -1,17 +1,21 @@
-import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
+import os
 
 # Define the path to the scripts inside the Docker container
 TRAIN_SCRIPT = '/opt/airflow/components/train.py'
 PREDICT_SCRIPT = '/opt/airflow/components/predict.py'
+EVALUATE_SCRIPT = '/opt/airflow/components/evaluate.py'
 
 def train_model():
     os.system(f'python {TRAIN_SCRIPT}')
 
 def predict_model():
     os.system(f'python {PREDICT_SCRIPT}')
+
+def evaluate_model():
+    os.system(f'python {EVALUATE_SCRIPT}')
 
 default_args = {
     'owner': 'airflow',
@@ -30,4 +34,9 @@ with DAG(dag_id='house_price_dag', default_args=default_args, schedule_interval=
         python_callable=predict_model
     )
     
-    train_task >> predict_task
+    evaluate_task = PythonOperator(
+        task_id='evaluate_model',
+        python_callable=evaluate_model
+    )
+    
+    train_task >> predict_task >> evaluate_task
